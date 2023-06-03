206dc7c7747e522107e6639610d69f9d1e984002
Samantha High, PhD <2448356+SamanthaHighPhD@users.noreply.github.com>
2023-06-02 20:18:31 -0700

    updated meta.tsx

--- blog-starter-app/components/meta.tsx
+++ blog-starter-app/components/meta.tsx
@@ -37,7 +37,7 @@ const Meta = () => {
         name="description"
         content={`Samantha High's blog.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:image" content="../public/assets/blog/authors/SH.jpg" />
     </Head>
   )
 }
